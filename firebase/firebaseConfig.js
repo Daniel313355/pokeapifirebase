db5dbcbc38c609f677fc8a16dc613ee40b1d4682
Daniel313355 <daniel.vieda@uniagustiniana.edu.co>
2025-05-28 00:45:09 +0000

    why?

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAh9Nn8d6gseb4Kdh6KdgMGKI2XfKxnepo",
+  authDomain: "pokeapifirebase-525c3.firebaseapp.com",
+  projectId: "pokeapifirebase-525c3",
+  storageBucket: "pokeapifirebase-525c3.firebasestorage.app",
+  messagingSenderId: "562245158914",
+  appId: "1:562245158914:web:06642dd3a62edba3c03f1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
